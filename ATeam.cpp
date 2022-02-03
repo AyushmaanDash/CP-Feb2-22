3d5d816fffd969985bac620521f66393c597f16d
Ayushmaan Dash <97537316+AyushmaanDash@users.noreply.github.com>
2022-02-03 13:25:52 +0530

    Update ATeam.cpp

--- ATeam.cpp
+++ ATeam.cpp
@@ -2,39 +2,21 @@
 using namespace std;
 int main()
 {
-    // int n, a, b, c, solve;
-    // solve = 0;
-    // cin >> n;
-    // for (int i = 0; i < n; i++)
-    // {
-    //     cin >> a;
-    //     cout << " ";
-    //     cin >> b;
-    //     cout << " ";
-    //     cin >> c;
-    //     if ((a + b + c) > 2)
-    //         solve++;
-    // }
-
-    // cout<<solve;
-    // first approach
-
     int n, solve,sum;
     solve = 0;
-    sum=0;
     int solution[3];
-    cin >> n;
+    cin>>n;
     for (int i = 0; i < n; i++)
     {
+        sum=0;
          for (int j = 0; j < 3; j++)
          {
              cin>>solution[j];
-             sum=+solution[j];
+             sum+=solution[j];
          }
-          if (sum>3)   
+          if (sum>1)   
             solve++;
     }
-
     cout<<solve;
     return 0;
 }
